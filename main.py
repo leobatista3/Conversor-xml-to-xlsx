182f802fff986ae73a5118c7f528ae2ae563a460
Leo Batista <brleors@gmail.com>
2024-02-06 21:36:40 -0300

    Script pronto

---
+++ main.py
@@ -0,0 +1,37 @@
+import xmltodict
+import os
+import json
+import pandas as pd
+
+def pegar_infos(nome_arquivo, valores):
+   # print(f"Pegou as info {nome_arquivo}")
+    with open(f'nfs/{nome_arquivo}', "rb") as arquivo_xml:
+        dic_arquivo = xmltodict.parse(arquivo_xml)
+        # print(json.dumps(dic_arquivo, indent=4))
+        
+        if "NFe" in dic_arquivo:
+            infos_nf = dic_arquivo["NFe"]['infNFe']
+        else:
+            infos_nf = dic_arquivo["nfeProc"]["NFe"]['infNFe']
+        numero_nota = infos_nf['@Id']
+        empresa_emissora = infos_nf['emit']['xNome']
+        nome_cliente = infos_nf['dest']['xNome']
+        endereco = infos_nf['dest']['enderDest']
+        if "vol" in infos_nf['transp']:
+            peso = infos_nf['transp']['vol']['pesoB']
+        else:
+            peso = "Não informado!"
+        valores.append([numero_nota, empresa_emissora, nome_cliente, endereco, peso])
+        
+
+lista_arquivos = os.listdir("nfs")
+
+colunas = ["numero_nota", "empresa_emissora", "nome_cliente", "endereco", "peso"]
+valores = []
+
+for arquivo in lista_arquivos:
+    pegar_infos(arquivo, valores)
+    #break
+
+tabela = pd.DataFrame(columns=colunas, data=valores)
+tabela.to_excel("LeoFiscal.xlsx", index=False)
